ecc667f77716eafeeeb48b3df0c52c00856d9d5b
Arthur van de Vondervoort <arthur.vandevondervoort@vanroey.be>
2026-02-16 15:05:27 +0100

    Set packageVersion to beta in PipelineInitialize.ps1 and add LinterCop.dll to customCodeCops in settings.json

--- .AL-Go/PipelineInitialize.ps1
+++ .AL-Go/PipelineInitialize.ps1
@@ -2,7 +2,7 @@ Param([Hashtable] $parameters)
 
 # Configuration
 $scriptUrl = "https://raw.githubusercontent.com/ALCops/AL-Go/v1.0.0/scripts/Install-ALCops.ps1"
-$packageVersion = ""          # "" = latest stable, "alpha", "beta", or "1.2.3"
+$packageVersion = "beta"          # "" = latest stable, "alpha", "beta", or "1.2.3"
 $targetFramework = ""
 
 # Download and run the installer script

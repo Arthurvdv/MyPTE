c5892f538280a92aa0c1cdf29d17350c4110e375
Arthur van de Vondervoort <arthur.van.de.vondervoort@gmail.com>
2026-04-30 10:09:19 +0200

    Add --yes flag to npx command for ALCops installation

--- .AL-Go/PipelineInitialize.ps1
+++ .AL-Go/PipelineInitialize.ps1
@@ -9,7 +9,7 @@ Write-Host "Installing ALCops analyzers..."
 Write-Host "  Output: $output"
 Write-Host "  Detect using: $detectUsing"
 
-npx @alcops/core download --output $output --detect-using $detectUsing
+npx --yes @alcops/core download --output $output --detect-using $detectUsing
 if ($LASTEXITCODE -ne 0) {
     throw "alcops download failed with exit code $LASTEXITCODE"
 }
